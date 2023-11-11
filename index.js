c34941350ab6accae803f53673c42eb7fb97b562
angelahack1 <angelakimichellle@hotmail.com>
2023-11-11 17:41:24 -0600

    Trying the contact form work, by angelahack1

--- index.js
+++ index.js
@@ -5,28 +5,10 @@ const http = require('http');
 const { MongoClient } = require('mongodb');
 
 const app = express();
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, './')));
-
-async function run(date_arg, time_arg, mail_arg, comment_arg, ip_arg, forwarded_ips) {
-    const url ='mongodb://angelalm:1164Louder@docdb-aixkare2.czszlu5pf7si.us-east-1.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=global-bundle.pem&retryWrites=false'
-    const dbName = 'artecnologia_db';
-    const client = new MongoClient(url);
-    const db = client.db(dbName);
-    const collection = db.collection('req_for_comments');
-    try {
-    await client.connect();
-    const db = client.db(dbName);
-    const r = await collection.insertOne({ date: date_arg, time: time_arg, email: mail_arg, comment: comment_arg, ip: ip_arg, ips: forwarded_ips });
-    console.log(r.insertedId);
-    await client.close();
-    } catch (error) {
-    console.error('Error running script:', error);
-    }
-}
-    
+  
 app.post('/contact_form', (req, res) => {
     const formData = req.body;
     const remote_ip = req.ip;
@@ -41,10 +23,24 @@ app.post('/contact_form', (req, res) => {
     console.log(ips, '\n');
     }
     console.log('Form data received at: (',dateFormatted,' ',timeFormatted,') in JSON format:', formData);
-    run(dateFormatted, timeFormatted, formData.email, formData.texto, remote_ip, forwarded_ips).catch(console.dir);
+
+    var client = MongoClient.connect(
+        'mongodb://angelalm:1164Louder@docdb-aixkare2.czszlu5pf7si.us-east-1.docdb.amazonaws.com:27017/artecnologia_db?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false',
+        {
+          tlsCAFile: 'global-bundle.pem'
+        },
+        function(err, client) {
+            if(err)
+                throw err;
+            db = client.db('artecnologia_db');
+            col = db.collection('req_for_comments');
+            col.insertOne({'date': dateFormatted, 'time': timeFormatted, 'email': formData.email, 'comment': formData.texto, 'ip': remote_ip, 'ips': forwarded_ips}, function(err, result) {
+                console.log(result);
+           });
+        });
+    client.close();
     res.redirect('index.html');
     });
-    
 
 app.listen(3000, function() {
     console.log('Server is running on port 3000');
