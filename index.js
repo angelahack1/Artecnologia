73cff86e31ff88116760992a69ac34ddbb593004
angelahack1 <angelakimichellle@hotmail.com>
2023-11-11 19:15:25 -0600

    Just deleting credentials

--- index.js
+++ index.js
@@ -10,37 +10,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, './')));
 
-function sendMail(to, subject, text) {
-    let transporter = nodemailer.createTransport({
-        host: 'email-smtp.us-east-1.amazonaws.com',
-        port: 587,
-        secure: false,
-        auth: {
-            user: 'angela.l.m@aixkare.com', // replace with your email
-            pass: '1164Louder!#' // replace with your email password
-        },
-        tls: {
-            rejectUnauthorized: false
-        }
-    });
-
-    // setup email data with unicode symbols
-    let mailOptions = {
-        from: '"AixKare" <angela.l.m@aixkare.com>',
-        to: to, // list of receivers
-        subject: subject, // Subject line
-        text: text, // plain text body
-    };
-
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message sent: %s', info.messageId);
-    });
-}
-
 app.post('/contact_form', (req, res) => {
     const formData = req.body;
     const remote_ip = req.ip;
@@ -56,7 +25,6 @@ app.post('/contact_form', (req, res) => {
     }
     console.log('Form data received at: (',dateFormatted,' ',timeFormatted,') in JSON format:', formData);
 
-    sendMail('angelakimichellle@hotmail.com', 'logging', 'About to get comment...');
     var client = MongoClient.connect(
         'mongodb://angelalm:1164Louder@docdb-aixkare2.czszlu5pf7si.us-east-1.docdb.amazonaws.com:27017/artecnologia_db?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false',
         {
@@ -69,12 +37,10 @@ app.post('/contact_form', (req, res) => {
             col = db.collection('req_for_comments');
             col.insertOne({'date': dateFormatted, 'time': timeFormatted, 'email': formData.email, 'comment': formData.texto, 'ip': remote_ip, 'ips': forwarded_ips}, function(err, result) {
                 console.log(result);
-                sendMail('angelakimichellle@hotmail.com', 'logging', result);
-                sendMail('angelakimichellle@hotmail.com', 'logging', err);
+                res.send(result);
                 client.close();
            });
         });
-        sendMail('angelakimichellle@hotmail.com', 'logging', 'Finished getting comment...');
     res.redirect('index.html');
     });
 
